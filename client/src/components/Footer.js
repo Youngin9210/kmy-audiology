b0b20b0916bd513950b2d43fa06111f178108015
Kyle <kyleyoung.9210@gmail.com>
2021-07-13 22:05:30 -0400

    removed footer folder and placed component into compnent file directly

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
 	return (
-		<div className="flex justify-center items-center h-16 bg-white beltoneBlue">
+		<div className="flex flex-wrap lg:flex-nowrap justify-center items-center h-auto bg-white beltoneBlue">
 			<p className="p-2">Copyright © 2021 KMY Audiology. All Rights Reserved</p>
 			<p className="p-2">Designed By KMY Dev</p>
 		</div>
