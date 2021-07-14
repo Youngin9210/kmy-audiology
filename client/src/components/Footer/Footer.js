3f45cb9bd8fcaf387973dfbc044170315c13873d
Kyle <kyleyoung.9210@gmail.com>
2021-07-13 20:41:16 -0400

    created basic footer for now

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className="flex justify-center items-center h-16 bg-white beltoneBlue">
+			<p>Copyright © 2021 KMY Audiology. All Rights Reserved</p>
+			<p>Designed By KMY Dev</p>
+		</div>
+	);
 }
