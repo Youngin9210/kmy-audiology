d847480b9e27f78a11367f0b9eca7bfffce9c6e3
Kyle <kyleyoung.9210@gmail.com>
2021-07-22 11:18:21 -0400

    adding some todos for footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function Footer() {
 	return (
 		<div className="flex flex-wrap lg:flex-nowrap justify-center items-center h-auto bg-white beltoneBlue">
-			<p className="p-2">Copyright © 2021 KMY Audiology. All Rights Reserved</p>
+			<p className="p-2">Copyright © 2021 KMY Audiology</p>
+			<p className="p-2">All Rights Reserved</p>
+			TODO: Make designed by into a link to portfolio
 			<p className="p-2">Designed By KMY Dev</p>
+			TODO: Add social media icon for facebook pages
 		</div>
 	);
 }
