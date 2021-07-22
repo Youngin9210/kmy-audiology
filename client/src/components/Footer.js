e586bea9372e6de7c194c4f3baa26f7019d03e34
Kyle <kyleyoung.9210@gmail.com>
2021-07-22 11:23:29 -0400

    commented out todos

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
 		<div className="flex flex-wrap lg:flex-nowrap justify-center items-center h-auto bg-white beltoneBlue">
 			<p className="p-2">Copyright © 2021 KMY Audiology</p>
 			<p className="p-2">All Rights Reserved</p>
-			TODO: Make designed by into a link to portfolio
+			{/* TODO: Make designed by into a link to portfolio */}
 			<p className="p-2">Designed By KMY Dev</p>
-			TODO: Add social media icon for facebook pages
+			{/* TODO: Add social media icon for facebook pages */}
 		</div>
 	);
 }
