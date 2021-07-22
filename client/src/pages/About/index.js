8d721d50d49deaeaa0eeb098c4e9eb27a139f645
Kyle <kyleyoung.9210@gmail.com>
2021-07-22 13:07:14 -0400

    created general card format for displaying about information from sanity.io

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import BlockContent from '@sanity/block-content-to-react';
+import imageUrlBuilder from '@sanity/image-url';
+import React, { useEffect, useState } from 'react';
+import sanityClient from '../../client';
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+	return builder.image(source);
+}
 
 const About = () => {
+	const [author, setAuthor] = useState(null);
+
+	useEffect(() => {
+		sanityClient
+			.fetch(
+				`*[_type == 'author']{
+      name,
+      bio,
+      'authorImage': image.asset->url
+    }`
+			)
+			.then((data) => setAuthor(data[0]))
+			.catch((err) => console.log(err));
+	}, []);
+
+	if (!author) return <div>Loading...</div>;
 	return (
-		<div>
-			<h1>About</h1>
-		</div>
+		<main className="container mx-auto text-center m-10">
+			<h1 className="text-6xl my-4">About Us</h1>
+			<div className="p-10">
+				<section className="container mx-auto flex flex-wrap justify-center items-start my-16 beltoneBlue">
+					<div className="lg:w-3/4 w-full lg:pr-3">
+						<div className="bg-white rounded-xl p-6">
+							<img
+								src={urlFor(author.authorImage).url()}
+								alt="Dr. Young"
+								className="rounded lg:w-5/6 mx-auto"
+							/>
+							<div className="text-lg flex flex-col justify-center container mx-auto">
+								<h1 className="text-3xl font-bold m-4">{author.name}</h1>
+								<div className="prose lg:prose-xl leading-relaxed">
+									<BlockContent
+										blocks={author.bio}
+										projectId="t3iwym3s"
+										dataset="production"
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		</main>
 	);
 };
 
