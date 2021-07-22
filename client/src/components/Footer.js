d0d38bc476480fb0174f95df530b100faf718596
Kyle <kyleyoung.9210@gmail.com>
2021-07-22 13:05:39 -0400

    added 'LLC' to copyright

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<div className="flex flex-wrap lg:flex-nowrap justify-center items-center h-auto bg-white beltoneBlue">
-			<p className="p-2">Copyright © 2021 KMY Audiology</p>
+			<p className="p-2">Copyright © 2021 KMY Audiology LLC</p>
 			<p className="p-2">All Rights Reserved</p>
 			{/* TODO: Make designed by into a link to portfolio */}
 			<p className="p-2">Designed By KMY Dev</p>
