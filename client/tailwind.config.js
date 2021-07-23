abd066f420a8391d6aedb2626a0146c6e096a3e7
Kyle <kyleyoung.9210@gmail.com>
2021-07-22 20:36:44 -0400

    added a custom color for beltone blue

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				beltone: {
+					50: '#f2f7fa',
+					100: '#e6eef4',
+					200: '#bfd5e4',
+					300: '#99bbd4',
+					400: '#4d89b4',
+					500: '#005694',
+					600: '#004d85',
+					700: '#00416f',
+					800: '#003459',
+					900: '#002a49',
+				},
+			},
+		},
 	},
 	variants: {
 		extend: {},
