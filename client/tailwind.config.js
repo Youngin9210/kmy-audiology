d76b64d7c02088b8566a1746ff6cd9310a43838f
Kyle <kyleyoung.9210@gmail.com>
2021-07-26 15:01:46 -0400

    added clear blue and orange from beltone's color pallete

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				beltone: {
+				beltoneBlue: {
 					50: '#f2f7fa',
 					100: '#e6eef4',
 					200: '#bfd5e4',
@@ -15,6 +15,30 @@ module.exports = {
 					700: '#00416f',
 					800: '#003459',
 					900: '#002a49',
+				},
+				beltoneClearBlue: {
+					50: '#f6fbfe',
+					100: '#edf7fd',
+					200: '#d2ebf9',
+					300: '#b6dff6',
+					400: '#80c7ef',
+					500: '#49afe8',
+					600: '#429ed1',
+					700: '#3783ae',
+					800: '#2c698b',
+					900: '#245672',
+				},
+				beltoneOrange: {
+					50: '#fdf8f3',
+					100: '#fbf2e8',
+					200: '#f5dec5',
+					300: '#efcaa3',
+					400: '#e2a35d',
+					500: '#d67b18',
+					600: '#c16f16',
+					700: '#a15c12',
+					800: '#804a0e',
+					900: '#693c0c',
 				},
 			},
 		},
