8556eb4b7b97c69d228eae489f51eb1dd48c5360
Kyle <kyleyoung.9210@gmail.com>
2021-07-26 15:21:34 -0400

    adding validation to inputs

--- client/kmyaudiologyblog/schemas/locations.js
+++ client/kmyaudiologyblog/schemas/locations.js
@@ -8,48 +8,57 @@ export default {
 			name: 'city',
 			title: 'City',
 			type: 'string',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 		{
 			name: 'state',
 			title: 'State',
 			type: 'string',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 		{
 			name: 'address',
 			title: 'Street Address',
 			type: 'string',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 		{
 			name: 'zip',
 			title: 'Zipcode',
 			type: 'number',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 
 		{
 			name: 'days',
 			title: 'Days Open (example: M-F or Tue & Th)',
 			type: 'string',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 		{
 			name: 'hours',
 			title: 'Business Hours (example: 9am-4pm or 9am-12pm | 1pm-4pm)',
 			type: 'string',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 		{
 			name: 'phone',
 			title: 'Business Phone Number',
 			type: 'string',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 		{
 			name: 'fax',
 			title: 'Business Fax',
 			type: 'string',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 		{
 			name: 'social',
 			title: 'Social Media Link (example: https://facebook.com/beltone)',
 			type: 'string',
 			initialValue: 'https://',
+			validation: (Rule) => Rule.required().error('Cannot be empty'),
 		},
 		{
 			name: 'image',
