f7f153c881f8a5e440ca5f5a3791926ede13a70f
Kyle <kyleyoung.9210@gmail.com>
2021-07-26 15:16:51 -0400

    author info in an es6 Logical AND operator

--- client/src/pages/Blog/index.js
+++ client/src/pages/Blog/index.js
@@ -36,18 +36,17 @@ const Blog = () => {
 	return (
 		<main className="min-h-screen p-12">
 			<section className="container mx-auto text-center">
-				<h1 className="text-6xl">Blog</h1>
-				<div className="flex flex-wrap items-start my-16 gap-8">
+				<h1 className="text-6xl text-beltone-800 font-bold">Blog</h1>
+				<div className="flex flex-wrap justify-center items-start my-16 gap-8">
 					{blogData &&
 						blogData.map((blog, index) => {
-							console.log('blog body:', blog.body);
 							return (
 								<article key={index} className="lg:w-1/4 w-full lg:px-3">
 									<Link
 										to={`/blog/${blog.slug.current}`}
 										key={blog.slug.current}
 									>
-										<div className="bg-gray-200 rounded-xl relative text-white">
+										<div className="rounded-xl relative text-white">
 											<div className="absolute top-0 bottom-0 left-0 right-0 h-full z-10 bg-gradient-to-b from-transparent to-beltone-900 rounded-b-xl"></div>
 											<img
 												src={blog.mainImage.asset.url}
@@ -58,16 +57,18 @@ const Blog = () => {
 												<h2 className="text-2xl text-left font-bold mb-2">
 													{blog.title}
 												</h2>
-												<div className="flex leading-relaxed">
-													<img
-														src={urlFor(blog.authorImage).url()}
-														alt={blog.name}
-														className="w-10 h-10 rounded-full"
-													/>
-													<p className="flex items-center pl-2 text-lg">
-														{blog.name}
-													</p>
-												</div>
+												{blog.name && (
+													<div className="flex leading-relaxed">
+														<img
+															src={urlFor(blog.authorImage).url()}
+															alt={blog.name}
+															className="w-10 h-10 rounded-full"
+														/>
+														<p className="flex items-center pl-2 text-lg">
+															{blog.name}
+														</p>
+													</div>
+												)}
 											</div>
 										</div>
 									</Link>
