02810c4f92a56228a89249a2a724c4b7b0ecfcf0
Kyle <kyleyoung.9210@gmail.com>
2021-07-26 15:18:04 -0400

    rendering blog info including author info

--- client/src/components/SingleBlog.js
+++ client/src/components/SingleBlog.js
@@ -22,11 +22,13 @@ const SingleBlog = () => {
         asset->{
           _id,
           url
-        }
+        },
+				alt
       },
       body,
       'name': author->name,
-      'authorImage': author->image
+      'authorImage': author->image,
+			publishedAt
     }`
 			)
 			.then((data) => setBlog(data[0]))
@@ -35,19 +37,42 @@ const SingleBlog = () => {
 
 	if (!blog) return <div>Loading...</div>;
 	return (
-		<main className="container mx-auto text-center m-10">
+		<main className="container mx-auto text-center my-6">
 			<div className="p-10">
-				<section className="container mx-auto flex flex-wrap justify-center items-start my-16 beltoneBlue">
+				<section className="container mx-auto flex flex-wrap justify-center items-start text-beltone-800">
 					<div className="lg:w-3/4 w-full lg:pr-3">
-						<div className="bg-white bg-opacity-75 rounded-xl p-6">
+						<div>
+							<h1 className="text-5xl font-bold">{blog.title}</h1>
+							{blog.name && (
+								<div className="flex justify-center mt-4">
+									<img
+										src={urlFor(blog.authorImage).url()}
+										alt={blog.name}
+										className="w-10 h-10 rounded-full"
+									/>
+									<p className=" text-2xl leading-relaxed flex justify-center items-center pl-2">
+										{blog.name}
+									</p>
+								</div>
+							)}
+							{blog.publishedAt && (
+								<p className="leading-relaxed mt-4">
+									{new Date(blog.publishedAt).toLocaleString('en-US', {
+										month: 'long',
+										day: 'numeric',
+										year: 'numeric',
+									})}
+								</p>
+							)}
+						</div>
+						<div className="p-6">
 							<img
 								src={urlFor(blog.mainImage).url()}
-								alt="Dr. Young"
+								alt={blog.mainImage.alt}
 								className="rounded lg:w-5/6 mx-auto"
 							/>
-							<div className="text-lg flex flex-col justify-center container mx-auto">
-								<h1 className="text-3xl font-bold m-4">{blog.title}</h1>
-								<div className="prose lg:prose-xl">
+							<div className="text-lg flex flex-col justify-center container mx-auto mt-12">
+								<div className="">
 									{blog.body.length <= 1 ? (
 										<p className="leading-relaxed text-left">
 											{blog.body[0].children[0].text}
@@ -64,16 +89,6 @@ const SingleBlog = () => {
 										})
 									)}
 								</div>
-							</div>
-							<div className="flex justify-center text-gray-800 mt-4">
-								<img
-									src={urlFor(blog.authorImage).url()}
-									alt={blog.name}
-									className="w-10 h-10 rounded-full"
-								/>
-								<p className="leading-relaxed flex justify-center items-center text-beltone-500 pl-2">
-									{blog.name}
-								</p>
 							</div>
 						</div>
 					</div>
