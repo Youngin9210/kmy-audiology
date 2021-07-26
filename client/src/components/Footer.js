3ad5301e144853a4a7bb96b6c0bb456626ac6ab9
Kyle <kyleyoung.9210@gmail.com>
2021-07-26 15:07:05 -0400

    removing beltoneBlue class

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div className="flex flex-wrap lg:flex-nowrap justify-center items-center h-auto bg-white beltoneBlue">
+		<div className="flex flex-wrap lg:flex-nowrap justify-center items-center h-auto bg-white">
 			<p className="p-2">Copyright © 2021 KMY Audiology LLC</p>
 			<p className="p-2">All Rights Reserved</p>
 			{/* TODO: Make designed by into a link to portfolio */}
